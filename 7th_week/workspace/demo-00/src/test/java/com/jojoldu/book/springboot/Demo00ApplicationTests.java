4f1c18a2ad70e95b6845f6a46bb6ac1049bb6c08
yunha woo <wjh0970@gmail.com>
2022-08-29 17:50:37 +0900

    28days commit

---
+++ 7th_week/workspace/demo-00/src/test/java/com/jojoldu/book/springboot/Demo00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
